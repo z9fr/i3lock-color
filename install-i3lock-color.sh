305da376f578fc9d5cc89904b4fdcd68b69d89ea
Raymond Li <hi@raymond.li>
2021-08-21 19:59:44 -0400

    Fix quotes

--- install-i3lock-color.sh
+++ install-i3lock-color.sh
@@ -7,7 +7,7 @@ cd build
 sudo make install
 sudo install -Dm644 ../LICENSE "$LICENSE"
 
-echo "i3lock-color installed. The binary and manpage listing are `i3lock`.
+echo "i3lock-color installed. The binary and manpage listing are \`i3lock'.
 The license can be found at $LICENSE
 GitHub repo: https://github.com/Raymo111/i3lock-color
 Discord server: https://discord.gg/FzVPghyDt2"
